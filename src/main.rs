1cef56f44c2421df6c47e4110846594ce4630ddd
Tim <trhksrc@Tims-MacBook-Pro.local>
2020-07-02 14:27:45 -0700

    update 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+// use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use clap::App;
+
+struct Args {
+    filename: String,
+    replacing_word: String,
+    replacement_word: String,
+}
+
+fn main() -> std::io::Result<()> {
+    // Get command line arguments using clap
+    let matches = App::new("word_replacement")
+        .version("1.0")
+        .author("Timothy Tso <trhksrc@protonmail.ch>")
+        .arg("-f, --filename=[FILE] 'Sets the text file'")
+        .arg("-w, --words...=[replacing_word][replacement_word] 'Sets the two words to be used to process the text'")
+        .get_matches();
+
+    // Get and process individual arguments
+    let filename: String = matches.value_of("filename").unwrap().to_string();
+    let mut words: Vec<_> = matches.values_of("words").unwrap().collect();
+
+    // Instantiate the args struct containing the three arguments
+    let args = Args {
+        filename: filename,
+        replacing_word: words.remove(0).to_string(),
+        replacement_word: words.remove(0).to_string(),
+    };
+
+    // Read text file
+    let mut txt_file = File::open(args.filename)?;
+    let mut txt = String::new();
+    txt_file.read_to_string(&mut txt)?;
+
+    // Split the text into a list of the words
+    let txt_list: Vec<&str> = txt.split_whitespace().collect();
+
+    // Loop through the text list and replace every replacing word with the replacement word
+    let mut proc_txt_list = Vec::new();
+
+    for word in &txt_list {
+        if word == &args.replacing_word {
+            proc_txt_list.push(args.replacement_word.to_owned());
+        } else {
+            proc_txt_list.push(word.to_string());
+        }
+    }
+
+    // Put the processed words into a single String
+    let mut proc_txt = String::new();
+
+    for word in &proc_txt_list {
+        proc_txt.push_str(word);
+        proc_txt.push_str(" ");
+    }
+    println!("{}", &proc_txt);
+
+    Ok(())
 }
